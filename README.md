6a2c66e720def6b373647b486ba70843b9c4ef61
Leoneider <leoneider@hotmail.es>
2024-03-26 17:51:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-simple-reactive-forms
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Leoneider/angular-simple-reactive-forms)
---
+++ src/app/billing-info-unnested/billing-info-unnested.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators,AbstractControl, ValidationErrors } from "@angular/forms";
+
+@Component({
+  selector: 'app-billing-info-unnested',
+  templateUrl: './billing-info-unnested.component.html',
+  styleUrls: ['./billing-info-unnested.component.css']
+})
+export class BillingInfoUnnestedComponent implements OnInit {
+
+// public nestedForm: FormGroup = new FormGroup({
+//   fname: new FormControl("", [Validators.required]),
+//   email: new FormControl("", [Validators.required, Validators.email]),
+// addressLine: new FormControl("", [Validators.required]),
+// areacode: new FormControl("", [Validators.required, Validators.maxLength(5)])
+// })
+
+public nestedForm: FormGroup = new FormGroup({
+  basicInfo: new FormControl(""),
+  address: new FormControl("")
+});
+
+basicInfo: FormControl = NE
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+public onSubmit(){
+  // if(this.nestedForm.invalid){
+  //   return
+  // }
+
+  console.log(" Billing Form", this.nestedForm);
+}
+}
